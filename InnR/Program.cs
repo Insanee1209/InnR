ccfa7240734d71e0c4ea571664d584b6d6a05b86
Insani1209 <innnnn.r12@gmail.com>
2025-10-23 20:05:01 +0700

    Updatein1

--- InnR/Program.cs
+++ InnR/Program.cs
@@ -8,6 +8,7 @@ namespace TestLab
         {
             
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insaniee!");
 
             
             string name = "John";
